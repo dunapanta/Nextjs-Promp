1aeea8fc9f5107759c392a1e10bf2386638b6349
Daniel Unapanta <dunapanta@outlook.es>
2023-05-22 22:06:58 -0500

    Add Home data

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover 6 Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="text-center desc">
+        Get inspired by our AI-powered prompts and discover, create and share
+        creative prompts.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
